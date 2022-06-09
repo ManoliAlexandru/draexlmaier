04e785e23cef878a4434d19ec82b1d6a73623368
ManoliAlexandru <sasha-manoli@mail.ru>
2022-06-10 02:38:33 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,97 @@
+// Navbar
+$(document).on("scroll", () => {
+  navbar.style.height = 80 - Math.min(Math.max(window.scrollY - 80, 0), 25) + 'px';
+
+  if (window.scrollY > 80) {
+    $("#navbar-button").removeClass("btn-dark");
+    $("#navbar-button").addClass("btn-minimized");
+    $("#navbar").removeClass("navbar-simple")
+    $("#navbar").addClass("navbar-whitebg")
+  } else {
+    $("#navbar-button").addClass("btn-dark");
+    $("#navbar-button").removeClass("btn-minimized");
+    $("#navbar").addClass("navbar-simple")
+    $("#navbar").removeClass("navbar-whitebg")
+    
+  }
+})
+
+// Scroll
+// window.addEventListener('scroll', event => {
+//   let nav = document.querySelector('.nav-container');
+
+//   (window.scrollY >= 20) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
+// });
+
+window.addEventListener('scroll', event => {
+  let navigationLinks = document.querySelectorAll('.navbar .nav-link');
+  let fromTop = window.scrollY;
+
+  navigationLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+});
+
+// Slider slick
+$('.slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// Google Maps
+// function initMap() {
+//   const location = { lat: -25.344, lng: 131.036 };
+
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: location,
+//   });
+
+//   const marker = new google.maps.Marker({
+//     position: location,
+//     map: map,
+//   });
+// }
+
+// wow.js
+new WOW().init();
